255581ad6c16a434d9bc729ac321e5b7f223ecbc
Ariel Cuesta <arielcc88@gmail.com>
2020-10-07 20:47:03 -0500

    uploading build content for react application.

---
+++ client/build/precache-manifest.12dac84abd107b188144e87ea996f2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67232f7124afdad786d4501eed836849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375ba955d7434ef105dd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "599cd3b15d29f399f334",
+    "url": "/static/js/2.f9c3ee30.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f9c3ee30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375ba955d7434ef105dd",
+    "url": "/static/js/main.211d23e2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
